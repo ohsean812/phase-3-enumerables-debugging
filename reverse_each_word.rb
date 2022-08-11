a9bb20937d0bae90e8b5a45a83bba7ccf44efd1d
Sean Oh <ohsean812@gmail.com>
2022-08-11 11:03:09 -0400

    commit tree 4b5931e01a9aad35cfe46c9766c4ef647f3b1e6b

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,1 +1,13 @@
-# Write your code here
+require 'pry'
+
+def reverse_each_word(sentence)
+    words = sentence.split
+    reversed_words = []
+
+    words.each do |word|
+        reversed_word = word.reverse
+        reversed_words << reversed_word
+    end
+end
+
+reverse_each_word("Hello there, and how are you?")
